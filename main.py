6793675779f7a59b374d5ace3068e3daa3195eed
Roy J. Harris III "Trey <Zithlos@gmail.com>
2023-01-23 11:39:18 -0600

    added function to draw spirograph

--- main.py
+++ main.py
@@ -29,13 +29,13 @@ def dotted_line():
 
 
 def draw_shapes():
+    soop.screen.colormode(1)
     # Draw overlaying shapes with decreasing sides
     soop.hideturtle()
     soop.penup()
     soop.goto(-75.00, -375.00)
     soop.showturtle()
     soop.pendown()
-    drawing = True
     sides = 16
     c = 0
     for num in range(2, 16):
@@ -45,7 +45,7 @@ def draw_shapes():
         soop.fillcolor(my_colors[c])
         angle = 360 / sides
         soop.begin_fill()
-        for i in range(0, sides):
+        for _ in range(0, sides):
             soop.forward(150)
             soop.left(angle)
         sides -= 1
@@ -54,19 +54,38 @@ def draw_shapes():
 
 
 def random_walk():
-    soop.pensize(5)
-    soop.speed(0)
-    soop.color(random.choice(my_colors))
-    direction = [90, 270, 180, 0]
-    to_go = random.choice(direction)
-    soop.seth(to_go)
-    soop.forward(random.randrange(20, 60))
+    for _ in range(0, 200):
+        soop.screen.colormode(255)
+        soop.pensize(5)
+        soop.speed(0)
+        soop.pencolor(random_color())
+        direction = [90, 270, 180, 0]
+        to_go = random.choice(direction)
+        soop.seth(to_go)
+        soop.forward(random.randrange(20, 60))
 
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, b, g)
+    return color
+
+
+def draw_spirograph():
+    soop.speed(0)
+    for _ in range(int(360 / 5)):
+        soop.screen.colormode(255)
+        soop.pencolor(255, 255, 255)
+        soop.circle(1, 5)
+        soop.pencolor(random_color())
+        soop.circle(150)
 
 # draw_square()
 # dotted_line()
 # draw_shapes()
-for _ in range(0, 200):
-    random_walk()
+# random_walk()
+draw_spirograph()
 screen = Screen()
 screen.exitonclick()
